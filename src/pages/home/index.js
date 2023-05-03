cca4c2898059c0c5c5088f963c2ed473ad140554
Fernando Suzin <suzin.dev@gmail.com>
2023-05-03 19:08:02 -0300

    Implementando página home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,15 +1,67 @@
-import { View, Text, StyleSheet } from "react-native";
+import { useState } from "react";
+import { View, Text, StyleSheet, SafeAreaView, TextInput, TouchableOpacity } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
+import { Logo } from "../../components/logo";
 
 export function Home() {
+    const [inputValue, setInputValue] = useState("")
+
+    function handlerSearch() {
+        console.log(inputValue);
+    }
+
     return (
-        <View style={styles.container}>
-            <Text>Págima Home!!!</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <Logo />
+
+            <Text style={styles.title}>Encontre a receita</Text>
+            <Text style={styles.title}>que combina com você</Text>
+
+            <View style={styles.form}>
+                <TextInput
+                    placeholder="Digite o nome da comida..."
+                    style={styles.input}
+                    value={inputValue}
+                    onChangeText={(text) => setInputValue(text)}
+                />
+                <TouchableOpacity onPress={handlerSearch}>
+                    <Ionicons name="search" size={28} color="#4CBE6C" />
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: 'red'
+        flex: 1,
+        backgroundColor: '#F3F9FF',
+        paddingTop: 36,
+        paddingStart: 14,
+        paddingEnd: 14
+    },
+    title: {
+        fontSize: 26,
+        fontWeight: 'bold',
+        color: "#0e0e0e"
+    },
+    form: {
+        backgroundColor: '#FFF',
+        width: '100%',
+        borderRadius: 8,
+        marginTop: 16,
+        marginBottom: 16,
+        borderWidth: 1,
+        borderColor: '#ECECEC',
+        paddingLeft: 8,
+        paddingRight: 8,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    input: {
+        width: '90%',
+        height: 54,
+        maxWidth: '90%'
     }
 })
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Home } from '../pages/home';
 import { Favorites } from '../pages/favorites';
+import { Ionicons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
@@ -22,10 +23,28 @@ export function Routes() {
             <Tab.Screen
                 name="HomeTab"
                 component={Home}
+                options={{
+                    tabBarIcon: ({ color, size, focused }) => {
+                        if (focused) {
+                            return <Ionicons name="home" color="#000" size={size} />
+                        }
+
+                        return <Ionicons name="home-outline" color={color} size={size} />
+                    }
+                }}
             />
             <Tab.Screen
                 name="Favorites"
                 component={Favorites}
+                options={{
+                    tabBarIcon: ({ color, size, focused }) => {
+                        if (focused) {
+                            return <Ionicons name="heart" color="#FF4141" size={size} />
+                        }
+
+                        return <Ionicons name="heart-outline" color={color} size={size} />
+                    }
+                }}
             />
         </Tab.Navigator>
     )
